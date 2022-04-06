a097dc31806479e6dd35c9003d69f5d29338cb0d
qiubaopeng <wy13192237903@163.com>
2022-04-06 22:38:32 +0800

    1

---
+++ src/main/java/com/itgodo/DemoGitHub20220406Application.java
@@ -0,0 +1,13 @@
+package com.itgodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitHub20220406Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitHub20220406Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/itgodo/DemoGitHub20220406ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itgodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitHub20220406ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
